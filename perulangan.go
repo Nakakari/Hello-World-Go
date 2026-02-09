e9c80560ce7e2527bdede491ae3b840ec5b62c9d
Nakakari <sarinandhika@gmail.com>
2026-02-09 20:21:04 +0700

    perulangan

--- perulangan.go
+++ perulangan.go
@@ -40,4 +40,56 @@ func main() {
 	for _, v := range zs {
 		fmt.Println("Value=", v)
 	}
+
+	var kvs = map[byte]int{'a': 0, 'b': 1, 'c': 2} //map
+	for k, v := range kvs {
+		fmt.Println("key=", k, "value=", v)
+	}
+
+	// boleh juga baik k dan atau v-nya diabaikan
+	for range kvs {
+		fmt.Println("done")
+	}
+
+	// selain itu bisa juga dengan cukup menentukan nilai numerik perulangan
+	for i := range 5 {
+		fmt.Println(i)
+	}
+
+	/*
+		Penggunaan keyword break & continue
+	*/
+	for i := 1; i <= 10; i++ {
+		if i%2 == 1 {
+			continue
+		}
+		if i > 8 {
+			break
+		}
+
+		fmt.Println("Angka", i)
+	}
+
+	/*
+		Perulangan bersarang.
+	*/
+	for i := 0; i < 5; i++ {
+		for j := i; j < 5; j++ {
+			fmt.Println(j, " ")
+		}
+
+		fmt.Println()
+	}
+
+	// Pemanfaatan label dalam perulangan
+outerloop:
+	for i := 0; i < 5; i++ {
+		for j := 0; j < 5; j++ {
+			if i == 3 {
+				break outerloop
+			}
+
+			fmt.Println("matriks [", i, "][", j, "]", "\n")
+		}
+	}
 }
